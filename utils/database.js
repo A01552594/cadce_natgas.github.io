a744015800b763b43e167a1d082df5a36a922195
Diego <A01552594@itesm.mx>
2021-04-26 10:01:19 -0500

    Pagina web

---
+++ utils/database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'bb8c6tkoblg2qoca2wqb-mysql.services.clever-cloud.com',
+    user: 'uwo0i7fedy6xkesb',
+    database: 'bb8c6tkoblg2qoca2wqb',
+    password: 'EofUZmotE7KtZ5eHhLy4'
+});
+
+module.exports = pool.promise();
+
+
+/*
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'natdev',
+    password: ''
+});
+
+
+*/
